600f96cb346c7b321d90ff4244bbb0995775a035
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-02-18 14:17:09 -0300

    feat: initialize repository

---
+++ backend/api-seu-cafezinho/src/test/java/com/seucafezinho/api_seu_cafezinho/ApiSeuCafezinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seucafezinho.api_seu_cafezinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeuCafezinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
