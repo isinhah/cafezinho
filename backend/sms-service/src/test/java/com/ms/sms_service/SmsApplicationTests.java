e0f9f9ac759fbcbfe2521036d664f9ed6c5b117d
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-04-03 12:31:47 -0300

    feat: add sms-service microservice

---
+++ backend/sms-service/src/test/java/com/ms/sms_service/SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.sms_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
